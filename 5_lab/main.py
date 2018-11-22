77bee5c2c03cb87e5aec9260b66a56ea7a102aeb
Nikita Kiselov <nikita.kiselov@outlook.com>
2018-11-22 10:28:50 +0200

    Critical fixes

--- 5_lab/main.py
+++ 5_lab/main.py
@@ -17,25 +17,27 @@ if __name__ == '__main__':
         temporary_solution_1 = list()
         temporary_solution_2 = list()
 
-        temporary_solution_1.append(get_length(w, abs(length_list[i] - solutions[i - 1][0]["l"])))
-        temporary_solution_1.append(get_length(w, abs(0 - solutions[i - 1][0]["l"])))
+        temporary_solution_1.append(get_length(w, abs(0 - solutions[i - 1][0]["l"])) + solutions[i - 1][0]["prev_dist"])
+        temporary_solution_1.append(get_length(w, abs(0 - solutions[i - 1][1]["l"])) + solutions[i - 1][1]["prev_dist"])
 
         if temporary_solution_1[0] > temporary_solution_1[1]:
-            final_solution_1 = {"l": length_list[i],
-                                "prev_dist": temporary_solution_1[0] + solutions[i - 1][0]["prev_dist"]}
+            final_solution_1 = {"l": 0,
+                                "prev_dist": temporary_solution_1[0]}
         else:
             final_solution_1 = {"l": 0,
-                                "prev_dist": temporary_solution_1[1] + solutions[i - 1][0]["prev_dist"]}
+                                "prev_dist": temporary_solution_1[1]}
 
-        temporary_solution_2.append(get_length(w, abs(length_list[i] - solutions[i - 1][1]["l"])))
-        temporary_solution_2.append(get_length(w, abs(0 - solutions[i - 1][1]["l"])))
+        temporary_solution_2.append(get_length(w, abs(length_list[i] - solutions[i - 1][0]["l"]))
+                                    + solutions[i - 1][0]["prev_dist"])
+        temporary_solution_2.append(get_length(w, abs(length_list[i] - solutions[i - 1][1]["l"]))
+                                    + solutions[i - 1][1]["prev_dist"])
 
         if temporary_solution_2[0] > temporary_solution_2[1]:
             final_solution_2 = {"l": length_list[i],
-                                "prev_dist": temporary_solution_2[0] + solutions[i - 1][1]["prev_dist"]}
+                                "prev_dist": temporary_solution_2[0]}
         else:
-            final_solution_2 = {"l": 0,
-                                "prev_dist": temporary_solution_2[1] + solutions[i - 1][1]["prev_dist"]}
+            final_solution_2 = {"l": length_list[i],
+                                "prev_dist": temporary_solution_2[1]}
 
         solutions.append([final_solution_1, final_solution_2])
 
